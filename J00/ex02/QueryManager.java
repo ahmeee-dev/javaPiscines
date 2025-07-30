4914bc7ac10ccc37712412a8a9170810083848ea
Ahmed Abdelrahman <ahmeabd36@gmail.com>
2025-07-30 11:19:31 +0200

    feat: completed ex02

---
+++ J00/ex02/QueryManager.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class QueryManager {
+	public static void main(String[] args) {
+		Scanner myScan = new Scanner(System.in);
+		
+		int counter = 0;
+
+		while (true) {
+		int query = myScan.nextInt();
+			if (query == 42) {
+				System.out.println(counter);
+				System.exit(0);
+			}
+			else if (primeCheck(query) == true)
+				counter++;
+		}
+
+	}
+
+	public static boolean primeCheck(int number) {
+		if (number <= 1){
+			System.err.println("IllegalArgument");
+			System.exit(-1);
+		}
+
+		int paragon = 2;
+		while (paragon * paragon < number) {
+			if ((number % paragon) == 0) {
+				return (false);
+			}
+			paragon++;
+		}
+		return (true);
+	}
+}
