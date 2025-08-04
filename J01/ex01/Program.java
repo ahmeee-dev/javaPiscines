cb5802cefd52add3650dce07f0bfc0bb7796b74e
Ahmed Abdelrahman <ahmeabd36@gmail.com>
2025-08-04 18:22:11 +0200

    feat: completed ex01

---
+++ J01/ex01/Program.java
@@ -0,0 +1,11 @@
+
+public class Program {
+	public static void main(String[] args) {
+
+			User Jam = new User("Jam", 2000);
+			User Leslie = new User("Les", 500);
+
+			System.out.println(Jam.getId());
+			System.out.println(Leslie.getId());
+	}
+}
