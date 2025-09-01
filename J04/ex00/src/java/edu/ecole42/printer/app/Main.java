5f52626716780c345ee42f02a7b41e5460f781c2
Ahmed Abdelrahman <ahmeabd36@gmail.com>
2025-09-01 16:26:13 +0200

    feat: exercise structure and implementation compelte

---
+++ J04/ex00/src/java/edu/ecole42/printer/app/Main.java
@@ -0,0 +1,34 @@
+package edu.ecole42.printer.app;
+import edu.ecole42.printer.logic.*;
+
+import java.awt.image.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+
+// reads input BMP file, returns an ArrayList<int> with the content of the file
+class Main {
+
+	public static void main(String[] args) {
+		
+		if (args.length != 1 || !args[0].endsWith(".bmp")) {
+			throw new IllegalArgumentException(); };
+
+		final char white = ' ';
+		final char black = '0';
+		File file;
+		BufferedImage img = null;
+		try {
+			file = new File(args[0]);
+			img = ImageIO.read(file);
+		} catch (Exception err) { err.printStackTrace();System.exit(2); }
+		Interpoler interpoler = new Interpoler(img, black, white);
+		interpoler.print();
+	}
+}
---
+++ J04/ex00/src/java/edu/ecole42/printer/logic/Interpoler.java
@@ -0,0 +1,28 @@
+package edu.ecole42.printer.logic;
+import java.awt.image.*;
+import java.rmi.UnexpectedException;
+
+public class Interpoler {
+	private final BufferedImage image;
+	private final char black;
+	private final char white;
+
+	public Interpoler(BufferedImage img, char black, char white) { this.image = img; this.black = black; this.white = white; }
+
+	public void print() {
+		try {
+			for (int x = 0; x < this.image.getWidth(); x++) {
+				for (int y = 0; y < this.image.getHeight(); y++) {
+					if (this.image.getRGB(x, y) == java.awt.Color.WHITE.getRGB()) {
+						System.out.print(this.white);
+					} else if (this.image.getRGB(x, y) == java.awt.Color.BLACK.getRGB()) {
+						System.out.print(this.black);
+					} else {
+						throw new Exception("Unexpected color in file"); 
+					};
+				}
+			} System.out.println();
+		} catch (Exception err) { System.out.println("Error: " + err.getMessage()); }
+	}
+		
+}
