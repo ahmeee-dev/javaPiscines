167f723e5283b1b1f9e3c1b8475e34a8e48abc78
Ahmed Abdelrahman <ahmeabd36@gmail.com>
2025-09-02 10:56:40 +0200

    feat: completed ex00 [tests = positive]

--- J04/ex00/src/java/edu/ecole42/printer/logic/Interpoler.java
+++ J04/ex00/src/java/edu/ecole42/printer/logic/Interpoler.java
@@ -11,15 +11,17 @@ public class Interpoler {
 
 	public void print() {
 		try {
-			for (int x = 0; x < this.image.getWidth(); x++) {
-				for (int y = 0; y < this.image.getHeight(); y++) {
+			for (int y = 0; y < this.image.getHeight(); y++) {
+				System.out.println();
+				for (int x = 0; x < this.image.getWidth(); x++) {
 					if (this.image.getRGB(x, y) == java.awt.Color.WHITE.getRGB()) {
 						System.out.print(this.white);
 					} else if (this.image.getRGB(x, y) == java.awt.Color.BLACK.getRGB()) {
 						System.out.print(this.black);
-					} else {
-						throw new Exception("Unexpected color in file"); 
-					};
+					} else { System.out.print("x"); }
+					//else {
+					//	throw new Exception("Unexpected color in file"); 
+					//};
 				}
 			} System.out.println();
 		} catch (Exception err) { System.out.println("Error: " + err.getMessage()); }
